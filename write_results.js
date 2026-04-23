d49124bb187d73eeb995cd790b3562f83cd45d10
Gabriel3025 <gabriel.desouza9@hotmail.com>
2026-04-23 09:39:42 -0300

    Auto-update: 2026-04-23 09:39:42

--- write_results.js
+++ write_results.js
@@ -7,19 +7,7 @@ const CREDENTIALS_PATH = 'C:/Users/Administrador.LAURAFERREIRA/Downloads/.gdrive
 const SHEET_ID = '1902H_f_1PpnA9M0E_MpHEYfavj4U-nwKGzurbvf8PYg';
 
 const results = [
-  { rowIdx: 47, colDia: 14, valor: 140 }, // Fichas e Resumos de Letras - DIA 9
-  { rowIdx: 48, colDia: 12, valor: 8 },   // Projeto Marcenaria - DIA 7
-  { rowIdx: 49, colDia: 12, valor: 0 },   // Bijuteria - DIA 7
-  { rowIdx: 50, colDia: 12, valor: 21 },  // Alfabetização - DIA 7
-  { rowIdx: 51, colDia: 12, valor: 140 }, // Creme AntRugas (DROP) - DIA 7
-  { rowIdx: 52, colDia: 12, valor: 22 },  // Atividades Copa do mundo - DIA 7
-  { rowIdx: 53, colDia: 12, valor: 63 },  // Calistenia asiática - DIA 7
-  { rowIdx: 54, colDia: 12, valor: 10 },  // Religião LATAM - DIA 7
-  { rowIdx: 55, colDia: 12, valor: 4 },   // Dinamicas terapeuticas - DIA 7
-  { rowIdx: 56, colDia: 9,  valor: 26 },  // Hora da Leiturinha - DIA 4
-  { rowIdx: 57, colDia: 9,  valor: 27 },  // EUAMOAnatomia - DIA 4
-  { rowIdx: 58, colDia: 9,  valor: 4 },   // Cafajeste (Acompanhar OF) - DIA 4
-  { rowIdx: 59, colDia: 8,  valor: 10 },  // Sono bebe - DIA 3
+  { rowIdx: 60, colDia: 15, valor: 0 },   // Sono bebe - DIA 10
 ];
 
 function colToLetter(col) {
